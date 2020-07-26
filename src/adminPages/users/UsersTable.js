c0fe3a0a084add2f9513d702cb6f42648592a6a5
Uriel <urielkelman@gmail.com>
2020-07-26 02:10:22 -0300

    Quitadas cosas innecesarias

--- src/adminPages/users/UsersTable.js
+++ src/adminPages/users/UsersTable.js
@@ -2,27 +2,9 @@ import React, {Component} from "react";
 
 import Button from "react-bootstrap/Button";
 import {FaPencilAlt, FaTrash} from "react-icons/fa";
-import CustomPagination from "../../common/CustomPagination";
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Table from "react-bootstrap/Table";
 import Pagination from "@material-ui/lab/Pagination";
-import Grid from "@material-ui/core/Grid";
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
 
 
 class UsersTable extends Component {
